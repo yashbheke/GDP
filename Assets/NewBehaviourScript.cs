4ad4b48bb3422c501e337d297f015938c7d33e5b
yashbheke <42708078+yashbheke@users.noreply.github.com>
2019-02-06 22:41:28 +0530

    Add files via upload

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public float speed = 2;
+    public float force = 300;
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+    }
+}
